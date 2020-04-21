b7dfc59eae6423a903135eeb58fe5bc6daa3d05f
Dibyadeep Saha <dibyadeepsaha.main@gmail.com>
2020-04-21 16:07:46 +0800

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,25 @@
+function calculate(){
+    var first = parseInt(document.getElementById("first").value);
+    var second = parseInt(document.getElementById("second").value);
+
+    if (document.getElementById('r1').checked) {
+        document.getElementById("num").innerHTML = first + " + " + second + " = " + parseInt(first + second)
+    }
+
+    else if (document.getElementById('r2').checked) {
+        document.getElementById("num").innerHTML = first + " - " + second + " = " + parseInt(first - second)
+    }
+
+    else if (document.getElementById('r3').checked) {
+        document.getElementById("num").innerHTML = first + " x " + second + " = " + parseInt(first * second)
+    }
+
+    else if (document.getElementById('r4').checked) {
+
+        if (second == 0) {window.alert("Warning: Denominator != 0")}
+
+        else { 
+            document.getElementById("num").innerHTML = first + " / " + second + " = " + (first / second)
+        }      
+    }
+}
